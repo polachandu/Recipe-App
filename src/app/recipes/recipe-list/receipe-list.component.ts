e169f90e625b4f9cd2253780e9398a809d486cfc
polachandu <polachandrahas@gmail.com>
2023-04-21 21:43:35 -0700

    Initial phase

--- src/app/recipes/recipe-list/receipe-list.component.ts
+++ src/app/recipes/recipe-list/receipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is a test recipe','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_R-p10o8_9IOGiXUnKXPK2e1K_uzf5TadtQ&usqp=CAU')
+  ];
 }
